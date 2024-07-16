feeb0114fccdffb7b3f96a5ba07d83018beb2f02
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2024-07-17 00:50:55 +0100

    Initial Commit

---
+++ src/app/pokemon/card/sample-card/sample-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-card',
+  templateUrl: './sample-card.component.html',
+  styleUrl: './sample-card.component.css'
+})
+export class SampleCardComponent {
+
+}
