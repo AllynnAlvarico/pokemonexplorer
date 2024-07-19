1888b626ff26194e55cc48e9bdd1f3c934ecbd86
Allynn Alvarico <allynn_alvarico@yahoo.com>
2024-07-19 13:33:05 +0100

    working on data service for pokemons

--- src/app/pokemon/card/card.component.ts
+++ src/app/pokemon/card/card.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../../service/data.service";
+import {response} from "express";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
-
+export class CardComponent implements OnInit{
+  pokemons: any[] = [];
+  name: string = '';
+  constructor(
+    private dataService: DataService
+  ) {
+  }
+  ngOnInit() {
+    this.dataService.getPokemons()
+      .subscribe((response: any) => {
+        console.log(response);
+        response.results.forEach((result: any) => {
+          this.dataService.getPokemonDetails(result.name)
+            .subscribe((uniqueResponse: any) => {
+              this.pokemons.push(uniqueResponse);
+              console.log(uniqueResponse.name);
+            });
+        });
+    });
+  }
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import * as http from "http";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService implements OnInit{
 
-  constructor() { }
+  api: string = 'https://pokeapi.co/api/v2/';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    console.log("hello");
+  }
+  getPokemons(){
+    return this.http.get(`${this.api}pokemon?limit=100000&offset=0`);
+  }
+  getPokemonDetails(name: string){
+    return this.http.get(`${this.api}/pokemon/${name}`)
+  }
+  getPokemonData(url: string){
+    return this.http.get(`${url}`);
+  }
+
+
+
+
 }
